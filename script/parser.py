acb5d401f654b4590438b22711521a40bc6b4f0c
Liza Ovchinnikova <elizusha@google.com>
2020-10-08 10:59:15 +0200

    Working implementation of scraper and parser

--- script/parser.py
+++ script/parser.py
@@ -13,15 +13,25 @@ from rdflib import Graph, URIRef, Literal
 
 def main():
     g = Graph()
-    paths = glob.glob("/home/elizusha/data_extractor/data/*")
+    paths = glob.glob("/home/elizusha/data_extractor/data/*.html")
     for path in paths:
-        file = open(path)
-        html = file.read()
-        jslde = JsonLdExtractor()
-        data = jslde.extract(html)
-        json_data = json.dumps(data)
-        g.parse(data=json_data, format="json-ld")
-    print(g.serialize(format="n3").decode("utf-8"))
+        print("HTML: ", path, end=" --- ")
+        with open(path) as file:
+            try:
+                html = file.read()
+                jslde = JsonLdExtractor()
+                data = jslde.extract(html)
+            except Exception as e:
+                print("Extraction error")
+                continue
+            json_data = json.dumps(data)
+            g.parse(data=json_data, format="json-ld")
+            print("OK")
+    print("++++++++++++++++++++++++++++++++++++++++++")
+    g_turtle = g.serialize(format="turtle").decode("utf-8")
+    print(g_turtle)
+    with open("../result.ttl", "w") as output:
+        output.write(g_turtle)
 
 
 if __name__ == "__main__":
--- script/scraper.py
+++ script/scraper.py
@@ -19,6 +19,7 @@ def parse_args():
 
 def main():
     args = parse_args()
+    main_url = urlparse(args.input_link)
     state_path = os.path.join(args.output_directory, "state.json")
     if not os.path.exists(state_path):
         with open(state_path, "w") as f:
@@ -28,48 +29,68 @@ def main():
                 "counter": 0,
             }
             f.write(json.dumps(new_state))
-    state = json.load(open(state_path))
+    with open(state_path) as json_file:
+        state = json.load(json_file)
     while state["new_pages"]:
         page = state["new_pages"].pop()
         print("CURRENT_PAGE:", page)
+        with open("../urls.txt", "a") as f:
+            f.write(page+"\n")
         page_url = urlparse(page)
+        file_name = str(state["counter"]) + ".html"
+        state["saved_pages"][page] = file_name
+        state["counter"] += 1
+        with open(state_path, 'w') as json_file:
+            json.dump(state, json_file)
         try:
-            file_name = str(state["counter"]) + ".html"
-            state["saved_pages"][page] = file_name
-            state["counter"] += 1
-            urllib.request.urlretrieve(
-                page, os.path.join(args.output_directory, file_name)
-            )
+            response = requests.get(page, timeout=60)
             time.sleep(1)
-            # save state
-            response = requests.get(page)
-            time.sleep(1)
-        except:
-            print(":(")
+        except Exception as e:
+            print("Site error")
+            print(e)
             continue
+        if not response.ok:
+             # print("Bad request")
+             continue
+        content_type = response.headers.get('Content-Type')
+        # print("###", content_type)
+        if content_type and not content_type.startswith("text/html"):
+            # print("Wrong format: ", content_type)
+            continue
+        with open(os.path.join(args.output_directory, file_name), 'w') as file:
+            file.write(response.text)
         soup = BeautifulSoup(response.text, "html.parser")
+
 
         for tag in soup.findAll("a"):
             if "href" in str(tag):
                 link = tag["href"]
-                print("#", link)
+                # print("#", link)
                 link_url = urlparse(link)
+                if link_url.scheme not in ["http", "https", ""]:
+                    # print(f"Link to different site: {link_url.scheme} != {page_url.scheme}")
+                    continue
+                if page_url.path and page_url.path[-1] == "/":
+                    new_path = os.path.join(page_url.path, link_url.path)
+                else:
+                    new_path = link_url.path
                 link_url = link_url._replace(
                     scheme=link_url.scheme or page_url.scheme,
                     netloc=link_url.netloc or page_url.netloc,
+                    path=os.path.normpath(new_path)
                 )
                 if link_url.netloc != page_url.netloc:
-                    print(f"Link to different site: {link_url.netloc} != {page_url.netloc}")
+                    # print(f"    Link to different site: {link_url.netloc} != {page_url.netloc}")
                     continue
                 link = link_url.geturl()
-                print("NORMALIZED_LINK:", link)
+                # print("NORMALIZED_LINK:", link)
                 if link in state["saved_pages"]:
-                    print(f"Link already saved")
+                    # print(f"    Link already saved")
                     continue
                 if link in state["new_pages"]:
-                    print(f"Link already in queue")
+                    # print(f"    Link already in queue")
                     continue
-                print("NEW_LINK:", link)
+                # print(" New link:", link)
                 state["new_pages"].append(link)
 
 
