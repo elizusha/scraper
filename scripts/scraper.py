507735a61cf151c06be111bc1d55f4ba397fa23e
Liza Ovchinnikova <elizusha@google.com>
2020-10-15 00:51:06 +0200

    scraper refactoring

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -9,115 +9,176 @@ import argparse
 from google.cloud import storage
 from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+from typing import NamedTuple, List, Dict, Optional, Any
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_link", help="site page")
-    parser.add_argument("output_dir", help="google cloud directory")
+    parser.add_argument("start_link", help="site page")
+    parser.add_argument("work_dir", help="google cloud directory")
     parser.add_argument(
         "--bucket_name", help="storage bucket", default="wikidata-collab-1-crawler"
+    )
+    parser.add_argument(
+        "--start_new", action="store_true", default=False, help="site page"
     )
     return parser.parse_args()
 
 
-def upload_blob(bucket_name, source_file, destination_blob_name):
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_string(source_file)
+class ScrapedPageInfo(NamedTuple):
+    file_name: Optional[str]
+    status_code: int
+    site_error: str
 
 
-def init_state(bucket, state_path, link):
-    state_blob = bucket.blob(state_path)
-    if not state_blob.exists():
-        return {
-            "saved_pages": {},
-            "new_pages": [link],
-            "counter": 0,
-        }
-    return json.loads(state_blob.download_as_string().decode())
+class ScraperState:
+    def __init__(
+        self,
+        counter: int,
+        page_queue: List[str],
+        processed_pages: Dict[str, ScrapedPageInfo],
+    ):
+        self.counter: int = counter
+        self.page_queue: List[str] = page_queue
+        self.processed_pages: Dict[str, ScrapedPageInfo] = processed_pages
+
+    @staticmethod
+    def from_json(json_str: str) -> "ScraperState":
+        json_data: Dict[str, Any] = json.loads(json_str)
+        return ScraperState(
+            counter=json_data["counter"],
+            page_queue=json_data["page_queue"],
+            processed_pages={
+                url: ScrapedPageInfo(**info_dict)
+                for url, info_dict in json_data["processed_pages"].items()
+            },
+        )
+
+    def to_json(self) -> str:
+        return json.dumps(
+            {
+                "counter": self.counter,
+                "page_queue": self.page_queue,
+                "processed_pages": {
+                    url: info._asdict() for url, info in self.processed_pages.items()
+                },
+            }
+        )
+
+    def get_next_filename(self) -> str:
+        self.counter += 1
+        return f"{self.counter}.html"
+
+    def add_processed_url(self, url: str, info: ScrapedPageInfo) -> None:
+        self.processed_pages[url] = info
+
+    def add_urls_to_queue(self, urls: List[str]) -> None:
+        for url in urls:
+            if url not in self.processed_pages and url not in self.page_queue:
+                # print(" New link:", link)
+                self.page_queue.append(url)
 
 
-def save_state(bucket, state_path, state):
-    state_blob = bucket.blob(state_path)
-    state_blob.upload_from_string(json.dumps(state))
+class Scraper:
+    def __init__(self, args):
+        self.storage_client = storage.Client()
+        self.bucket = self.storage_client.bucket(args.bucket_name)
+        self.work_dir = args.work_dir
+        self.start_link = args.start_link
+        self.start_new = args.start_new
 
+    @property
+    def state_path(self):
+        return os.path.join(self.work_dir, "state.json")
 
-def update_links_log(file_name, page_link, dir):
-    with open(f"../{dir}.txt", "a") as log:
-        log.write(f"{file_name} {page_link}\n")
+    def _init_state(self) -> ScraperState:
+        state_blob = self.bucket.blob(self.state_path)
+        if self.start_new or not state_blob.exists():
+            return ScraperState(0, [self.start_link], {})
+        return ScraperState.from_json(state_blob.download_as_string().decode())
+
+    def _save_state(self, state: ScraperState) -> None:
+        state_blob = self.bucket.blob(self.state_path)
+        state_blob.upload_from_string(state.to_json())
+
+    def extract_urls(self, text, page_str):
+        urls = []
+        page_url = urlparse(page_str)
+        soup = BeautifulSoup(text, "html.parser")
+        for tag in soup.findAll("a"):
+            if "href" not in str(tag):
+                continue
+            link = tag["href"]
+            # print("#", link)
+            link_url = urlparse(link)
+            if link_url.scheme not in ["http", "https", ""]:
+                # print(f"Link to different site: {link_url.scheme} != {page_url.scheme}")
+                continue
+            if page_url.path and page_url.path[-1] == "/":
+                new_path = os.path.join(page_url.path, link_url.path)
+            else:
+                new_path = link_url.path
+            link_url = link_url._replace(
+                scheme=link_url.scheme or page_url.scheme,
+                netloc=link_url.netloc or page_url.netloc,
+                path=os.path.normpath(new_path),
+            )
+            if link_url.netloc != page_url.netloc:
+                # print(f"    Link to different site: {link_url.netloc} != {page_url.netloc}")
+                continue
+            url = link_url.geturl()
+            urls.append(url)
+            # print("NORMALIZED_LINK:", link)
+        return urls
+
+    def _upload_blob(self, source_file, destination_file_name):
+        blob = self.bucket.blob(
+            os.path.join(self.work_dir, "html_data", destination_file_name)
+        )
+
+        blob.upload_from_string(source_file)
+
+    def scrape(self):
+        state = self._init_state()
+        while state.page_queue:
+            page = state.page_queue.pop()
+            print("CURRENT_PAGE:", page)
+
+            response = requests.get(page, timeout=60)
+            time.sleep(1)
+
+            file_name = None
+            site_error = None
+            urls = []
+
+            if response.ok:
+                content_type = response.headers.get("Content-Type")
+                # print("###", content_type)
+                if content_type and not content_type.startswith("text/html"):
+                    # print("Wrong format: ", content_type)
+                    site_error = f"Wrong format: {content_type}"
+                else:
+                    file_name = state.get_next_filename()
+                    self._upload_blob(response.text, file_name)
+                    urls = self.extract_urls(response.text, page)
+
+            state.add_processed_url(
+                page,
+                ScrapedPageInfo(
+                    file_name=file_name,
+                    status_code=response.status_code,
+                    site_error=site_error,
+                ),
+            )
+            state.add_urls_to_queue(urls)
+            self._save_state(state)
 
 
 def main():
     args = parse_args()
 
-    state_path = os.path.join(args.output_dir, "state.json")
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(args.bucket_name)
-
-    state = init_state(bucket, state_path, args.input_link)
-    while state["new_pages"]:
-        page = state["new_pages"].pop()
-        print("CURRENT_PAGE:", page)
-        page_url = urlparse(page)
-        file_name = str(state["counter"]) + ".html"
-
-        state["saved_pages"][page] = file_name
-        state["counter"] += 1
-        save_state(bucket, state_path, state)
-
-        try:
-            response = requests.get(page, timeout=60)
-            time.sleep(1)
-        except Exception as e:
-            print("Site error")
-            print(e)
-            continue
-        if not response.ok:
-            # print("Bad request")
-            continue
-        content_type = response.headers.get("Content-Type")
-        # print("###", content_type)
-        if content_type and not content_type.startswith("text/html"):
-            # print("Wrong format: ", content_type)
-            continue
-        output_file = os.path.join(args.output_dir, "html_data", file_name)
-        upload_blob("wikidata-collab-1-crawler", response.text, output_file)
-        update_links_log(file_name, page, args.output_dir)
-        soup = BeautifulSoup(response.text, "html.parser")
-
-        for tag in soup.findAll("a"):
-            if "href" in str(tag):
-                link = tag["href"]
-                # print("#", link)
-                link_url = urlparse(link)
-                if link_url.scheme not in ["http", "https", ""]:
-                    # print(f"Link to different site: {link_url.scheme} != {page_url.scheme}")
-                    continue
-                if page_url.path and page_url.path[-1] == "/":
-                    new_path = os.path.join(page_url.path, link_url.path)
-                else:
-                    new_path = link_url.path
-                link_url = link_url._replace(
-                    scheme=link_url.scheme or page_url.scheme,
-                    netloc=link_url.netloc or page_url.netloc,
-                    path=os.path.normpath(new_path),
-                )
-                if link_url.netloc != page_url.netloc:
-                    # print(f"    Link to different site: {link_url.netloc} != {page_url.netloc}")
-                    continue
-                link = link_url.geturl()
-                # print("NORMALIZED_LINK:", link)
-                if link in state["saved_pages"]:
-                    # print(f"    Link already saved")
-                    continue
-                if link in state["new_pages"]:
-                    # print(f"    Link already in queue")
-                    continue
-                # print(" New link:", link)
-                state["new_pages"].append(link)
+    scraper = Scraper(args)
+    scraper.scrape()
 
 
 if __name__ == "__main__":
